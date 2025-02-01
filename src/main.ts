a98f018e03e727b6f29007ce69c4814cc950cb3c
ruannilton <ruannilton@gmail.com>
2025-02-01 02:14:57 -0300

    update default port

--- src/main.ts
+++ src/main.ts
@@ -22,6 +22,6 @@ async function bootstrap() {
     allowedHeaders:
       'Origin, X-Requested-With, Content-Type, Accept, Authorization',
   });
-  await app.listen(process.env.PORT ?? 3030);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
